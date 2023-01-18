e83ca8f39fb26c920e764e6f3cf87b830a76583a
Winston <winston0803@cchntek.com>
2023-01-18 14:58:42 +0800

    [chore] add request trace logger

---
+++ app/logger/router_logger.go
@@ -0,0 +1,55 @@
+package logger
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+	"net/http/httputil"
+	"runtime/debug"
+	"time"
+)
+
+func GinLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+		logger.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
+		)
+	}
+}
+
+func GinRecovery(stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if stack {
+					logger.Error("recovery from panic",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					logger.Error("recovery from panic",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
+	}
+}
--- app/router/api.go
+++ app/router/api.go
@@ -1,7 +1,18 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"archilltect-sigma/app/logger"
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func Setup(r *gin.Engine) {
+	if os.Getenv("ENV") == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, "hello world")
+	})
 }
